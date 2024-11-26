a1651871d5139b983fca9286e026120f5e3c2d52
Mamadou Kaba <23130811+mdkaba@users.noreply.github.com>
2024-11-26 08:54:58 -0500

    Temporary save of changes

--- snapcaption/app/main.py
+++ snapcaption/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from router_Endpoints import router
+from app.router_Endpoints import router
+
 
 app = FastAPI()
 app.add_middleware(
